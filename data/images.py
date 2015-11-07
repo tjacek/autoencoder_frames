40fb52471512b0d11ee53a338613cbd8fa2f5539
tjacek <tjacek@student.agh.edu.pl>
2015-11-07 21:08:44 +0100

    read images

---
+++ data/images.py
@@ -0,0 +1,28 @@
+import re,pandas as pd 
+import imp
+utils =imp.load_source("utils","../utils.py")
+
+def read_image_frame(dir_path):
+    paths=utils.get_paths(dir_path,dirs=True)
+    images=[]
+    for cat,path in enumerate(paths):
+	cat_imgs=utils.read_img_dir(path)
+        cat_imgs=[standard_image(cat,img) for img in cat_imgs]
+        images+=cat_imgs
+    return create_img_frame(images)
+
+def create_img_frame(images):
+    labels=[ img[0] for img in images]
+    flat_imgs=[ img[1] for img in images]
+    return  pd.DataFrame({ 'Images':flat_imgs,
+                           'Category':labels,})
+
+def standard_image(cat,img):
+    img=img.flatten()
+    img/=max(img)
+    return cat,img
+
+if __name__ == "__main__":
+    path="../../imgs"
+    df=read_image_frame(path)
+    print(df.head())
