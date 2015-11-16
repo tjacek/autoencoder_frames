88f8ac4dcbb7960fba2c9c37229bba65b21fca81
tjacek <tjacek@student.agh.edu.pl>
2015-11-16 16:38:23 +0100

    reconstruct pca img

--- unsuperv/linear.py
+++ unsuperv/linear.py
@@ -14,6 +14,9 @@ class PcaReduction(object):
     def transform(self,x):
         return self.pca.transform(x)
 
+    def inverse_transform(self,x):
+        return self.pca.inverse_transform(x)
+
     def get_components(self,k):
         return self.pca.components_[0:k]
 
@@ -38,8 +41,8 @@ class PcaDecorator(object):
 
     def get_image(self,x):
         x_proj=self.pca.transform(x)
-        x_hid=self.autoencoder.transform(x_proj)
-        
+        x_hid=self.autoencoder.get_image(x_proj)
+        return self.pca.inverse_transform(x_hid)
 
     def get_model(self):
         ae_model=self.autoencoder.model
@@ -49,4 +52,3 @@ class PcaModel(object):
     def __init__(self,ae_model,nn_model):
         self.ae_model=ae_model
         self.nn_model=nn_model
-   
