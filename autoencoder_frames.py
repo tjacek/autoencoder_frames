e6c33707b574a0afa5ce6932620e8fffde0bf012
tjacek <tjacek@student.agh.edu.pl>
2015-11-18 16:04:26 +0100

    create composite cls

--- autoencoder_frames.py
+++ autoencoder_frames.py
@@ -5,19 +5,17 @@ import deep.nn as nn
 import deep.composite as comp
 import series
 
-def create_cls(img_path,out_path):
+def create_cls(img_path,ae_path,out_path):
     img_frame=images.read_image_frame(img_path)
     n_cats=data.get_n_cats(img_frame)
     print(n_cats)
-    cls=nn.built_nn_cls(n_cats)
+    cls=comp.create_extractor(n_cats,ae_path)
+    #cls=nn.built_nn_cls(n_cats)
     cls=deep.learning_iter(img_frame,cls,n_epochs=1000)
-    utils.save_object(cls,out_path)
+    utils.save_object(cls.get_model(),out_path)
     return cls
 
-def action_imgs(action_path,cls_path,out_path):
-    action_frame=acts.read_action_frame(action_path)
-    cls=comp.read_composite(cls_path)#utils.read_object(cls_path)
-    convert_actions(action_frame,cls)
+def action_imgs(action_frame,out_path):
     utils.make_dir(out_path)
     actions=action_frame['Action']
     act_imgs=[ (action.name,action.to_action_img()) for action in actions]
@@ -31,6 +29,7 @@ def action_features(action_path,cls_path,out_path):
     extractor=series.trivial_extr
     labeled_vectors=series.extract_features(action_frame,extractor)
     utils.to_labeled_file(out_path,labeled_vectors)
+    return action_frame
 
 def convert_actions(action_frame,cls):
     actions= action_frame['Action']
@@ -38,11 +37,10 @@ def convert_actions(action_frame,cls):
 
 if __name__ == "__main__":
     img_path="../imgs/"
-    cls_path="../nn/comp1"
+    cls_path="../nn/comp3"
+    ae_path="../nn/ae3"
     action_path="../large_/"
     out_path="../action_imgs/"
-    #cls=create_cls(img_path,cls_path)
-    #utils.read_object()
-    #action_imgs(action_path,cls_path,out_path)
-    action_features(action_path,cls_path,"../result/af.lb")
-    #deep.check_prediction(img_frame,cls)
+    #cls=create_cls(img_path,ae_path,cls_path)
+    af=action_features(action_path,cls_path,"../result/af.lb")
+    action_imgs(af,out_path)
