d3b7482c89ecce11759b2dff641a6b02110d5637
tjacek <tjacek@student.agh.edu.pl>
2015-12-02 09:33:36 +0100

    uniform frame size

---
+++ preproc/projections.py
@@ -0,0 +1,69 @@
+import utils
+import numpy as np
+import scipy.misc as image
+import point_cloud as pc
+
+DIRS=['xy/','zx/','zy/']
+
+class ProjectionAction(object):
+    def __init__(self,frames):
+        self.frames = frames
+
+    def scaled_action(self):
+        scaled_frames=[frame.scaled_frame() for frame in self.frames]
+        return ProjectionAction(scaled_frames)
+
+    def save(self,path):
+        utils.make_dir(path)
+    	for proj_type in DIRS:
+    	    utils.make_dir(path+'/'+proj_type)
+        for i,frame in enumerate(self.frames):
+            frame.save(path,'act'+str(i))
+
+class ProjectionFrame(object):
+    def __init__(self,projections):
+        self.projections=projections   
+
+    def scaled_frame(self):
+        start_dims=[proj.shape for proj in self.projections]
+        end_dims=[(60,60),(60,60),(60,60)]
+        new_dims=[new_dim(d0,d1) for d0,d1 in zip(start_dims,end_dims)]    
+        p_clouds=[pc.create_point_cloud(img,True) for img in self.projections]
+        for p_cloud,dim in zip(p_clouds,new_dims):
+            p_cloud.rescale(dim) 
+        print( p_clouds[0].find_max())
+        projections=[p_cloud.to_img(dim) for p_cloud,dim in zip(p_clouds,end_dims)]
+        return ProjectionFrame(projections)
+
+    def save(self,path,name):
+        for proj,postfix in zip(self.projections,DIRS):
+            proj_path=path+"/"+postfix
+            utils.make_dir(proj_path)
+            full_path=proj_path+name
+            print(full_path)
+            utils.save_img(full_path,proj)
+
+def new_dim(d0,d1):
+    x=float(d0[0])/float(d1[0])
+    y=float(d0[1])/float(d1[1])
+    return (x,y)
+
+def read_img_action(path):
+    raw_names=utils.get_files(path+"xy/")
+    names_size=len(raw_names)
+    names=["act"+str(i)+".png" for i in range(names_size)]
+    names=[utils.get_name(frame_path) for frame_path in names]
+    print(names)
+    new_proj=[read_projection_frame(path,name) for name in names]
+    return ProjectionAction(new_proj)
+
+def read_projection_frame(frame_path,name):
+    frame_paths=[ frame_path+prefix+name  for prefix in DIRS]
+    projs=[image.imread(path) for path in frame_paths]
+    return ProjectionFrame(projs)
+
+if __name__ == "__main__":
+    action_path="../show/"
+    action=read_img_action(action_path)
+    scaled_action=action.scaled_action()
+    scaled_action.save("../show/")
