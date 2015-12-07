218d33afb8541d22f9bae4e7cad8f2607d426efb
tjacek <tjacek@student.agh.edu.pl>
2015-12-07 19:08:21 +0100

    id of frames

--- preproc/projections.py
+++ preproc/projections.py
@@ -15,10 +15,11 @@ class ProjectionAction(object):
 
     def save(self,path):
         utils.make_dir(path)
+        name=utils.get_name(path)
     	for proj_type in DIRS:
     	    utils.make_dir(path+'/'+proj_type)
         for i,frame in enumerate(self.frames):
-            frame.save(path,'act'+str(i))
+            frame.save(path,name+str(i))
 
 class ProjectionFrame(object):
     def __init__(self,projections):
@@ -31,7 +32,6 @@ class ProjectionFrame(object):
         p_clouds=[pc.create_point_cloud(img,True) for img in self.projections]
         for p_cloud,dim in zip(p_clouds,new_dims):
             p_cloud.rescale(dim) 
-        print( p_clouds[0].find_max())
         projections=[p_cloud.to_img(dim) for p_cloud,dim in zip(p_clouds,end_dims)]
         return ProjectionFrame(projections)
 
@@ -49,18 +49,24 @@ def new_dim(d0,d1):
     return (x,y)
 
 def read_img_action(path):
-    raw_names=utils.get_files(path+"xy/")
-    names_size=len(raw_names)
-    names=["act"+str(i)+".png" for i in range(names_size)]
+    names=utils.get_files(path+"xy/")
+    #names_size=len(raw_names)
+    #names=["act"+str(i)+".png" for i in range(names_size)]
     names=[utils.get_name(frame_path) for frame_path in names]
     print(names)
     new_proj=[read_projection_frame(path,name) for name in names]
     return ProjectionAction(new_proj)
 
-def read_projection_frame(frame_path,name):
+def read_projection_frame(frame_path,name,normal=True):
     frame_paths=[ frame_path+prefix+name  for prefix in DIRS]
-    projs=[image.imread(path) for path in frame_paths]
+    projs=[read_img(path) for path in frame_paths]
     return ProjectionFrame(projs)
+
+def read_img(path):
+    img=image.imread(path)
+    img=img.astype(float)
+    img=img/255.0
+    return img
 
 if __name__ == "__main__":
     action_path="../show/"
