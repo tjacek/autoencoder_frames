c6efdc74176a16198dd529a4ce99bd02521a8e34
tjacek <tjacek@student.agh.edu.pl>
2015-12-22 22:51:31 +0100

    skeleton of sda

---
+++ deep/sda.py
@@ -0,0 +1,15 @@
+class SdA(object):
+ 	def __init__(self, arg):
+ 		self.arg = arg
+
+    def train(self,img,label):
+        return None
+
+    def get_features(self,img):
+        return None
+
+    def get_model(self):
+        return None
+
+    def get_category(self,img):
+        return None	 
