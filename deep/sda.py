cb5b1eaef844e608d972d6e382485273f5ee4393
tjacek <tjacek@student.agh.edu.pl>
2015-12-23 23:14:24 +0100

    model created

--- deep/sda.py
+++ deep/sda.py
@@ -1,6 +1,6 @@
 class SdA(object):
- 	def __init__(self, arg):
- 		self.arg = arg
+ 	def __init__(self, ae_layer,hidden,logistic):
+ 		self.model = SdaModel(ae_layer,hidden,logistic)
 
     def train(self,img,label):
         return None
@@ -12,4 +12,13 @@ class SdA(object):
         return None
 
     def get_category(self,img):
-        return None	 
+        return None
+
+class SdaModel(object):
+    def __init__(self,ae_layer,hidden,logistic):
+    	self.ae_layer=ae_layer
+        self.hidden=hidden
+        self.logistic=logistic
+        
+    def get_params(self):
+        return self.hidden.get_params() + self.logistic.get_params()	 
