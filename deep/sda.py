0db541b42ddabe4c5eab77e2ba7b31706dac1a2d
tjacek <tjacek@student.agh.edu.pl>
2015-12-30 22:58:47 +0100

    multilayer network

--- deep/sda.py
+++ deep/sda.py
@@ -12,7 +12,7 @@ class SdaModel(object):
         self.logistic=logistic
         
     def get_params(self):
-        params=self.ae_layer.model.get_params()
+        params=self.ae_layer.get_params()
         params+=self.hidden.get_params()
         params+=self.logistic.get_params()  
         return params  
@@ -28,18 +28,17 @@ def built_sda_cls(n_cats,ae_path,hyper_params=None):
 
 def create_sda_model(hyper_params):
     n_in=hyper_params['n_in']
+    n_ae=hyper_params['n_ae']
     n_hidden=hyper_params['n_hidden']
     n_out=hyper_params['n_out']
-    ae_path=hyper_params['ae_path']
-    ae=autoencoder.read_autoencoder(ae_path)
+    #ae=autoencoder.read_autoencoder(ae_path)
     rand=deep.RandomNum()
-    hidden_shape=(n_in,n_hidden)
-    hidden=deep.create_layer(hidden_shape,rand,"_hidden")
-    vis_shape=(n_hidden,n_out)
-    logistic=deep.create_layer(vis_shape,rand,"_vis")
-    return SdaModel(ae,hidden,logistic)
+    ae_layer=deep.create_layer((n_in,n_ae),rand,"_ae")
+    hidden=deep.create_layer((n_ae,n_hidden),rand,"_hidden")
+    logistic=deep.create_layer((n_hidden,n_out),rand,"_vis")
+    return SdaModel(ae_layer,hidden,logistic)
 
 def get_hyper_params(n_cats=2,n_in=3600,learning_rate=0.05):
     params={'learning_rate': learning_rate,'ae_path':"placeholder",
-            'n_in':n_in,'n_out':n_cats,'n_hidden':300}
+            'n_in':n_in,'n_ae':600,'n_hidden':300,'n_out':n_cats}
     return params
