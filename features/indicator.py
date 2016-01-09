a467854ad96ba51c19ac7be4e5a5241ea99bbf7b
tjacek <tjacek@student.agh.edu.pl>
2016-01-09 17:43:10 +0100

    new indicator

--- features/indicator.py
+++ features/indicator.py
@@ -5,7 +5,6 @@ def extract_indicator_features(in_path,out_path):
     action_t_series=utils.read_dir_objects(in_path)
     features=[from_instance(action) for action in action_t_series]
     utils.to_labeled_file(out_path,features)
-    print(features)
 
 def from_instance(action):
     arrays=action.to_array()
@@ -17,36 +16,38 @@ def from_instance(action):
     return features,str(action.cat)+"#"+str(action.person)
 
 def indicator_features(cats):
-    indc_extr=[be_indicator]
+    indc_extr=[af_indicator,cc_indicator,dd_indicator,cb_indicator]
     features=[extr_i(cats) for extr_i in indc_extr]
     return features
 
-def be_indicator(cats):
-    print(cats)
+def cb_indicator(cats):
     size=cats.size
     count=0.0
     for i in range(0,size-1):
-        if(cats[i]==1 and cats[i+1]==4):
+        if(cats[i]==2 and cats[i+1]==1):
             count=1.0
     return count    
 
-def cat2_indicator(cats):
+def af_indicator(cats):
     size=cats.size
     count=0.0
-    for i in range(1,size-1):
-        if(cats[i]==0 and cats[i-1]==1 and cats[i+1]==1):
-            count+=1.0
-    return count
+    for i in range(0,size-1):
+        if(cats[i]==0 and cats[i+1]==5):
+            count=1.0
+    return count  
 
-def fr12_indicator(cats):
-	if(cats[12]==0):
-		return 1.0
-	return 0.0
-
-def isolated_a(cats):
+def cc_indicator(cats):
     size=cats.size
     count=0.0
-    for i in range(1,size-1):
-        if(cats[i]==0 and cats[i-1]!=0 and cats[i+1]!=0):
-            count+=1.0
-    return count
+    for i in range(0,size-1):
+        if(cats[i]==2 and cats[i+1]==2):
+            count=1.0
+    return count 
+
+def dd_indicator(cats):
+    size=cats.size
+    count=0.0
+    for i in range(0,size-1):
+        if(cats[i]==3 and cats[i+1]==3):
+            count=1.0
+    return count 
