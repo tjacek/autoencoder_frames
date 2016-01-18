aa047a0f99ec024494ade7f19783b07995598296
tjacek <tjacek@student.agh.edu.pl>
2016-01-18 22:40:28 +0100

    parse args

--- test_cls.py
+++ test_cls.py
@@ -2,18 +2,19 @@ import utils
 import data
 import deep.sda as sda
 import numpy as np
+import sys
 
 def show_category(dim,size,params):
     out_path=params['out_path']
     utils.make_dir(out_path)
+    actions=data.read_actions(params['action_path'])
+    extr=sda.read_sda(params['cls_path'],params['conf_path'])
     for i in range(size):
         full_path=out_path+"cls"
         print(full_path)
-        apply_cls(dim,i,params)
+        apply_cls(dim,i,params,actions,extr)
 
-def apply_cls(dim,s_cat,params):
-    actions=data.read_actions(params['action_path'])
-    extr=sda.read_sda(params['cls_path'],params['conf_path'])
+def apply_cls(dim,s_cat,params,actions,extr):
     imgs=data.get_named_projections(dim,actions)
     img_cats=[]
     for img_i in imgs: 
@@ -31,10 +32,17 @@ def apply_cls(dim,s_cat,params):
 
 def get_params():
     return {'action_path':"../_final_actions/",
-            'cls_path':"../cascade4/nn/cls/large",
-            'conf_path':"../cascade4/cls_config/xy",
+            'cls_path':"../cascade5/nn/cls/large",
+            'conf_path':"../cascade5/cls_config/xy",
             'out_path':"../out/"}
 
+def parse_args(args):
+    if(len(args)>1):
+        cats=int(args[1])
+    else:
+        cats=20
+    return cats
 
 if __name__ == "__main__":
-    show_category(0,17,get_params())
+    cats=parse_args(sys.argv)
+    show_category(0,cats,get_params())
